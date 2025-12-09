697981f10fa1a6d386b0b367d71601f0cedb42dc
Saurabh Kushwaha <saurabhkushwaha9889@gmail.com>
2025-12-09 16:48:48 +0530

    Fix: Statuscodes improved

--- src/utils/global-error-handler/globalErrorHandler.ts
+++ src/utils/global-error-handler/globalErrorHandler.ts
@@ -3,12 +3,9 @@ import { ApiError, ApiResponse } from "../api-response/ApiResponse";
 import { NextFunction, Request, Response } from "express";
 
 export default function globalErrorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-    console.log("error is", err);
-    if(err instanceof ApiResponse){
-        return err;
-    }
+    console.log("Global Error handler:", err);
     if(err instanceof ApiError){
-        return ApiResponse.error(res, err.message, StatusCodes.INTERNAL_SERVER_ERROR, err.errors)
+        return ApiResponse.error(res, err.message, err.statusCode, err.errors)
     }
     return ApiResponse.error(res, err.message, StatusCodes.INTERNAL_SERVER_ERROR)
 }
